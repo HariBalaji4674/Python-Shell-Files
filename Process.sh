6c739ad22978c71c4682b1251033ff125369d045
HariBalaji4674 <Hari.Balaji4674@gmail.com>
2023-10-22 19:12:38 +0530

    Add New Files To The Repository

---
+++ Process.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Shell Commands with linux based 
+
+#To view The CPU Usage The commands used are 
+
+top >> content.txt
+
+htop >> content.txt
+
+free -h >> content.txt
--- Python.sh
+++ Python.sh
@@ -16,3 +16,12 @@ Inbuilt Functions:
 ------------------
 Split Function--> Separate the String into two parts and makes its as list 
 [1,2,3]
+
+String Inbuilt Functions:
+
+Split()
+Upper()
+Lower()
+isDigit()
+isNumeric()
+
