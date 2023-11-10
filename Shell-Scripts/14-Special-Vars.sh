7c4bf7c01d83e15d68eb98e5f7332eb5f6ef79f3
HariBalaji4674 <Hari.Balaji4674@gmail.com>
2023-11-10 22:03:09 +0530

    Adding All Files for for the first Commit

--- Shell-Scripts/14-Special-Vars.sh
+++ Shell-Scripts/14-Special-Vars.sh
@@ -9,3 +9,10 @@ echo "var2 = ${var2}"
 echo "No of Arguments = $#"
 echo "All Arguments = $@"
 echo "Script Name is = $0"
+ScriptName=$0
+Date=$(date +%F)
+LogFile=/tmp/$ScriptName-$Date.log
+
+echo "My Script Name is = ${LogFile}"
+echo "Date is = ${Date}"
+echo "Script Name = ${ScriptName}
