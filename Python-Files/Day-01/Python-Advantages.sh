6013656ff3dd2c960296dd92ae2d7f9df3d63421
HariBalaji4674 <Hari.Balaji4674@gmail.com>
2023-11-16 20:58:06 +0530

    Adding All Files for for the first Commit

---
+++ Python-Files/Day-01/Python-Advantages.sh
@@ -0,0 +1,25 @@
+Python Language:
+    Python is established in 1980 s
+    Python is Open Source
+    Python is High Level Programming Language
+    Python is Platform Independent
+    Python is Declarative Language and Scripted Language
+Python is Suaitable for DataScience,AI,ML,NLP
+We can Develop Desktop Application ,Web Apps,Mobile Applications.
+
+History :
+	1989 introduced Guido Van Rossum
+
+Installing and Downloading Python:
+	python .org
+	pycharm --> Editor
+	
+How Many ways we can run Python Code:
+	IDLE Shell --> Default Python Editor
+	CMD --> Python --> Run python.py
+	Notepad++ --> Can also run
+	Pycharm --> Python Charm editor
+	
+First Python Program:
+print("Hello World")
+	
