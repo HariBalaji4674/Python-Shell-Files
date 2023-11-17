1127afa130fb2a70bcb403aedb401b28821aa738
HariBalaji4674 <Hari.Balaji4674@gmail.com>
2023-11-17 21:36:42 +0530

    Adding All Files for for the first Commit

---
+++ Python-Files/Day-02/input.py
@@ -0,0 +1,9 @@
+# Taking The Input from the user
+# Type Conversion
+
+Number1 = input(" Enter First Number : ")
+print(Number1)
+
+# Every Thing passing through the terminal in python is string
+
+print(type(Number1))
