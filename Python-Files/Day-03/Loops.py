d3148209d6bd18daca8b63d52514ce9b01b3a660
HariBalaji4674 <Hari.Balaji4674@gmail.com>
2023-11-20 21:43:43 +0530

    Adding All Files for for the first Commit

--- Python-Files/Day-03/Loops.py
+++ Python-Files/Day-03/Loops.py
@@ -1,2 +1,43 @@
 # Control Statements:
-    
+# Looping Statements:
+# Loop is a Block of statements repeat several times
+# Range Function:
+
+
+x = range(10)
+print(x)
+y = range(20)
+print(y)
+
+z = range(0,20,3)
+print(z)
+
+for i in z:
+    print(i)
+
+for j in z:
+    print(j)
+
+for i in range(20):
+    print(i)
+
+print(list(range(10)))
+
+print(list(range(1,20,5)))
+
+print(list(range(0,10,3)))
+
+print(list(range(10,0,-1)))
+# Number Upto Numeber 10 Increment or decrement 2
+
+# Looping Statements:
+    # While Loop
+    # For Loop
+# While Loop
+# initialization
+# condition
+# incrementation
+i =1
+while i<=10:
+    print(i)
+    i = i+1
