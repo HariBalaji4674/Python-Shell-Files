4b31d3e01c04ff85e9345dc3c3947132e74782f0
HariBalaji4674 <Hari.Balaji4674@gmail.com>
2023-12-15 09:26:52 +0530

    adding new files

---
+++ Python-Files/OOPs/01-Class.py
@@ -0,0 +1,26 @@
+class Item:
+    pass
+
+item1 = Item()
+item1.name = 'phone'
+item1.price = 100
+item1.quantity = 5
+
+'''
+Functions
+Modules
+Procedural Programming language
+
+OOPs Concepts --> Object Programming Language:
+-----------------------------
+
+Object Have 2 Things: Attribute & Behaviours
+
+Attribute ==  data
+behaviour ==  how data works
+Class ==  design of objects (Blue-Print)
+Variables == store somethings
+methods ==  where to use those behaviour
+
+'''
+class computer
---
+++ Python-Files/OOPs/04-Polymorph.py
@@ -0,0 +1,17 @@
+import ABC, abstarctmethod
+
+'''
+Polymorphism : Poly Means Many Form
+Duck Typing
+Methods Overriding
+Method Overloading
+Operator Over Loading
+'''
+class Ducktypinh:
+    def main(self):
+        print("This is Main Methdod ")
+
+'''
+Abstract Methods
+
+'''
